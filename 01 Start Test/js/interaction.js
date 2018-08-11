890fbe51eea3bc3015ab6e46c2fa92637873fc52
darshangit <darshans1989@gmail.com>
2018-08-11 15:31:06 +0530

    custom animation + final tocuhes

--- 01 Start Test/js/interaction.js
+++ 01 Start Test/js/interaction.js
@@ -1,20 +1,56 @@
+var animationEnd = (function(el) {
+  var animations = {
+    animation: 'animationend',
+    OAnimation: 'oAnimationEnd',
+    MozAnimation: 'mozAnimationEnd',
+    WebkitAnimation: 'webkitAnimationEnd'
+  };
+
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+})(document.createElement('div'));
+
 // Page Load Function
 function pageLoaded() {
   //Btn 1 event
   $('.section1Btn').on('click', function() {
-    $('.section1').toggleClass('animated infinite bounce');
+    $('.section1').toggleClass('animated slideInDash');
+    //to check the animation end
+    $('.section1').on(animationEnd, removeAni1);
   });
 
   $('.section2Btn').on('click', function() {
     $('.section2').toggleClass('animated shake');
+    $('.section2').on(animationEnd, removeAni2);
   });
 
   $('.section3Btn').on('click', function() {
     $('.section3').toggleClass('animated tada');
+    $('.section3').on(animationEnd, removeAni3);
   });
 }
 
+function removeAni1() {
+  $('.section1').removeClass('animated slideInDash');
+  $('.section2').toggleClass('animated shake');
+  $('.section2').on(animationEnd, removeAni2);
+}
+
+function removeAni2() {
+  $('.section2').removeClass('animated shake');
+  $('.section3').toggleClass('animated tada');
+  $('.section3').on(animationEnd, removeAni3);
+}
+function removeAni3() {
+  $('.section3').removeClass('animated tada');
+}
+function removeAni4() {
+  $('.section4').removeClass('animated bounce');
+}
 
 animate4 = () => {
-    $('.section4').toggleClass('animated jello');
-}
+  $('.section4').toggleClass('animated jello');
+};
--- 02 Start Project/js/interaction.js
+++ 02 Start Project/js/interaction.js
@@ -1,4 +1,56 @@
+$.fn.extend({
+    animateCss: function(animationName, callback) {
+      var animationEnd = (function(el) {
+        var animations = {
+          animation: 'animationend',
+          OAnimation: 'oAnimationEnd',
+          MozAnimation: 'mozAnimationEnd',
+          WebkitAnimation: 'webkitAnimationEnd',
+        };
+  
+        for (var t in animations) {
+          if (el.style[t] !== undefined) {
+            return animations[t];
+          }
+        }
+      })(document.createElement('div'));
+  
+      this.addClass('animated ' + animationName).one(animationEnd, function() {
+        $(this).removeClass('animated ' + animationName);
+  
+        if (typeof callback === 'function') callback();
+      });
+  
+      return this;
+    },
+  });
+
+
 // Page Load Function
-function pageLoaded(){
-	
+function pageLoaded() {
+
+    //Section 1
+    $(".section1").on('click', () => {
+        $(".section1").animateCss('bounce', function () {
+            $(".section2").animateCss('bounce', function(){
+                $(".section3").animateCss('bounce')
+            })
+        })
+    });
+
+    //Section 2
+    $(".section2").on('click', () => {
+    })
+
+    //Section 3
+    $(".section3").on('click', () => {
+    })
+
+    //Section 4
+    $(".section4").on('click', () => {
+    })
+
+    //phone
+    $(".phone").on("mouseenter", () => {
+    })
 }
