53fcf1edad309e2c7f44dd48f8e646038ed37862
darshangit <darshans1989@gmail.com>
2018-08-11 09:08:52 +0530

    animations using jquery

--- 01 Start Test/js/interaction.js
+++ 01 Start Test/js/interaction.js
@@ -1,4 +1,20 @@
 // Page Load Function
-function pageLoaded(){
-	
+function pageLoaded() {
+  //Btn 1 event
+  $('.section1Btn').on('click', function() {
+    $('.section1').toggleClass('animated infinite bounce');
+  });
+
+  $('.section2Btn').on('click', function() {
+    $('.section2').toggleClass('animated shake');
+  });
+
+  $('.section3Btn').on('click', function() {
+    $('.section3').toggleClass('animated tada');
+  });
+}
+
+
+animate4 = () => {
+    $('.section4').toggleClass('animated jello');
 }
